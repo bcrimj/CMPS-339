2ff316d89b29cea3dd93b27322bc1d8c65dddf59
Logan Klier <logan.klier@selu.edu>
2022-07-09 16:43:17 -0500

    wip: added toast package

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@
 
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Basenav from "./Components/Navbar";
 import Alldata from "./screens/Alldata";
@@ -10,10 +15,12 @@ import Userscreen from "./screens/Userscreen";
 import Products from "./screens/Products";
 import logo from "./images/lion.png";
 import MyOrders from "./screens/Orders";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div>
+      <Toaster position="top-right" />
       <div>
         <img className="Lion" alt="A Lion" src={logo} />
       </div>
@@ -21,12 +28,11 @@ function App() {
         <Basenav />
         <Routes>
           <Route path="/" element={<Alldata />} />
-           
-            <Route path="user" element={<Userscreen />} />
-            <Route path="products" element={<Products />} />
-            <Route path="orders" element={<MyOrders />} />
-            <Route path='*' element={ <Navigate replace to="/"/>}/>
-          
+
+          <Route path="user" element={<Userscreen />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<MyOrders />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </Router>
     </div>
